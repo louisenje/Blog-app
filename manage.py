6209e53b91cc21a84fccb7181517780745657fd9
louisenje <louisenje@gmail.com>
2021-05-06 09:20:22 +0300

    added shell manager

--- manage.py
+++ manage.py
@@ -13,3 +13,20 @@ manager.add_command('server',Server)
 # migrations setts
 migrate=Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+# Tests 
+@manager.command
+def test():
+    """
+    run the unt tests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+# shell
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role,Blog=Blog,Comments=Comments)
+
+if __name__ == '__main__':
+    manager.run()
