9105b4bfff90689799aa481b5ac68a5edaf3310a
louisenje <louisenje@gmail.com>
2021-05-06 09:30:03 +0300

    initialized application and flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -15,3 +15,29 @@ mail=Mail()
 db=SQLAlchemy()
 bootstap=Bootstrap()
 
+photos = UploadSet('photos',IMAGES)
+def create_app(config_name):
+    #INitializing Application
+    app=Flask(__name__)
+
+    # creating the application configurations
+    app.config.from_object(config_options[config_name])
+    
+    # Initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    bootstap.init_app(app)
+    mail.init_app(app)
+      # configure UploadSet
+    configure_uploads(app,photos)
+    #REGISTERING THE BLUEPRINT
+       #main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+        # auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+
+    
+    
+    return app
